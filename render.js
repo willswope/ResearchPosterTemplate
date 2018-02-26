c631227df51c74211c96149e9763766554b88de0
willswope <36016830+willswope@users.noreply.github.com>
2018-02-26 11:34:14 -0600

    Update render.js

--- render.js
+++ render.js
@@ -4,7 +4,7 @@ page.viewportSize = { width: 5184, height: 3456 };
 //the clipRect is the portion of the page you are taking a screenshot of
 page.clipRect = { top: 0, left: 0, width: 5184, height: 3456 };
 //the rest of the code is the same as the previous example
-page.open('file:///C:/Users/Will/poster/poster.html', function() {
+page.open('FILE PATH HERE', function() {
   page.render('poster.png');
   phantom.exit();
 });
